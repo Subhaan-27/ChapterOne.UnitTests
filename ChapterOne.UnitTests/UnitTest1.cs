1549c19a9672b57081f63a8d2583ef91ae06e071
Subhaan-27 <36763210+Subhaan-27@users.noreply.github.com>
2019-03-05 13:18:28 +0100

    YES

--- ChapterOne.UnitTests/UnitTest1.cs
+++ ChapterOne.UnitTests/UnitTest1.cs
@@ -1,4 +1,7 @@
+using Microsoft.VisualStudio.TestPlatform.ObjectModel.Utilities;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TestDesign;
+
 
 namespace ChapterOne.UnitTests
 {
@@ -8,6 +11,72 @@ namespace ChapterOne.UnitTests
         [TestMethod]
         public void TestMethod1()
         {
+            var stringToCheck = "mysterious";
+
+            var stringToFind = "y";
+
+            var expectedResult = 1;
+
+            var classUnderTest = new TestDesign.StringUtilities();
+
+            var actualResult = classUnderTest.CountOccurences(stringToCheck, stringToFind[0]);
+            
+            Assert.AreEqual(expectedResult, actualResult);
         }
+
+
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            var stringToCheck = "mysterious";
+
+            var stringToFind = "s";
+
+            var expectedResult = 2;
+
+            var classUnderTest = new TestDesign.StringUtilities();
+
+            var actualResult = classUnderTest.CountOccurences(stringToCheck, stringToFind[0]);
+
+            Assert.AreEqual(expectedResult, actualResult);
+
+        }
+
+        [TestMethod]
+        public void TestMethod3()
+        {
+            var stringToCheck = "mySterious";
+
+            var stringToFind = "s";
+
+            var expectedResult = 2;
+
+            var classUnderTest = new TestDesign.StringUtilities();
+
+            var actualResult = classUnderTest.CountOccurences(stringToCheck, stringToFind[0]);
+
+            Assert.AreEqual(expectedResult, actualResult);
+
+        }
+
+        [TestMethod]
+        public void TestMethod4()
+        {
+            string stringToCheck = null;
+
+            var stringToFind = "s";
+
+            var expectedResult = -1;
+
+            var classUnderTest = new TestDesign.StringUtilities();
+
+            var actualResult = classUnderTest.CountOccurences(stringToCheck, stringToFind[0]);
+
+            Assert.AreEqual(expectedResult, actualResult);
+
+        }
+
+
     }
 }
---
+++ TestDesign/StringUtilities.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestDesign
+{
+   public class StringUtilities
+    {
+        public int CountOccurences(string stringToCheck, char stringToFind)
+        {
+
+            if (stringToCheck == null)
+            {
+                return -1;
+            }
+
+            else
+            {
+                    return stringToCheck.ToUpper().ToLower().Count(s => s == stringToFind);
+            }
+            
+        }
+
+
+    }
+}
